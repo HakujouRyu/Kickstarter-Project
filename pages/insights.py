82b052c2f89db9fc2089b28611f2bf9688405cd0
HakujouRyu <47827212+HakujouRyu@users.noreply.github.com>
2019-10-24 18:20:13 -0400

    Insert text in process and insights pages

--- pages/insights.py
+++ pages/insights.py
@@ -3,35 +3,70 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-
+ 
 from app import app
-
+ 
 column1 = dbc.Col(
     [
         dcc.Markdown(
             """
         
             ## What do we think?
-            Could this be useful? Well, short answer is: Maybe.  
-            It's interesting to me that 'Category' rose to the top of the feature's importance list everytime. 
+            Could this be useful? Well, the short answer is: Maybe.  
+            It's interesting to me that 'Category' rose to the top of the feature's importance list every time. 
             Sure, it makes sense in some ways, and we could speculate about who's on the internet, what categories are popular, etc. 
             But I don't think we could really formulate any conclusions on it from this data. 
             ## Speaking of Categories.
-            [Permutation_Importance](assets\\bigimportances.png){: .center-block :}
             ##### Let's talk features.
-            As you saw on the front page, there are many aspects of a Kickstarter campaign that affect it's outcome.
+            As you saw on the front page, there are many aspects of a Kickstarter campaign that affect its outcome.
             We could not observe them all in the scope of this project, such as how popular the creator is, 
             how much money they spend on advertising, etc.
-            There was plenty to look at still, and some of the other features that were shown to most heavily effect the model 
+            There was plenty to look at still, and some of the other features that were shown to most heavily affect the model 
             were: 
             - Your goal; seems pretty straight forward. Is yours too lofty?
             - Days from creation to launch; How long did you give yourself to hype up your project once you created it's "draft?"
-            - Campaing length; Kickstarter states that 30 days seems to be ideal for most.
+            - Campaign length; Kickstarter states that 30 days seems to be ideal for most.
             
             ## But how did we do?
             This is still a good dive into the subject, however.
             The baseline prediction accuracy rate of my 2019 data set was 57%.
             With this model, I was able to achieve an accuracy of 78%.
+            """
+        ),
+        html.Img(src='assets/truepos_small.png', className='img-fluid'),
+        
+        dcc.Markdown(
+            """
+            Here, we see where the model was correct in its prediction that this project would be successful.
+            By a large margin too. Note that the category of 'Video Games' carries a lot of weight!
+ 
+            """
+        ),
+        html.Img(src='assets/true_neg_small.png', className='img-fluid'),
+        
+        dcc.Markdown(
+            """
+            Here, we see another correct example. 
+            This time with the prediction that this project would fail.
+            Though the model just barely got this wrong, it may be worth noting that the days to launch was 0.
+            Would this project have been successful if they had spent more time tweaking the campaign publicly?
+ 
+            """
+        ),
+        html.Img(src='assets/false_pos_small.png', className='img-fluid'),
+        
+        dcc.Markdown(
+            """
+            Here is an interesting example, The model predicted that this one would succeed but the project failed.
+            Notice that the category even here was a big impact on the model's decision. Was it not enough, are we missing an angle,
+             or are we over/undervaluing another feature?
+ 
+            """
+        ),
+ 
+        dcc.Markdown(
+            """
+ 
             ## Why so low? 
             Well, as I stated before, I think some of the biggest features would be your advertising and previous success and/or fame.
             ## What now?
@@ -40,23 +75,32 @@ column1 = dbc.Col(
             and from there we could engineer a feature that is whether or not they have been successful in the past. 
             They may prove very predictive.
             Another feature that I did not get to engineer in my week of cleaning the blurb. 
-            What does the creator say about their project? Using NLP we could see if some blurbs are sugeested to be better than others. 
-
-
-
-
-
+            What does the creator say about their project? Using NLP we could see if some blurbs are suggested to be better than others. 
+ 
+ 
+ 
+ 
+ 
             """
         ),
     ],
-    md=12,
+    md=8,
 )
-
-
+ 
+ 
 column2 = dbc.Col(
     [
-        
+        html.Img(src='assets/small_importances.png', className='img-fluid'),
+        dcc.Markdown(
+            """These are the features used and their importances from tha latest iteration of the predictive model."""
+        ),
+        html.Img(src='assets/bigimportances.png', className='img-fluid'),
+        dcc.Markdown(
+            """ Here is what was passed to the very first iteration of the XGBClassifier that I used """
+        )
+ 
     ]
 )
+ 
+layout = dbc.Row([column1, column2])
 
-layout = dbc.Row([column1])
--- pages/process.py
+++ pages/process.py
@@ -3,21 +3,61 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-
+ 
 from app import app
-
+ 
 column1 = dbc.Col(
     [
         dcc.Markdown(
             """
         
-            ## Process
-
-
+            # Process
+            ### What did I spend a week doing? 
+            Cleaning mostly. When I first found this data set, I must admit, I was a little overzealous.
+            I saw so much opportunity, I did not take the time to look to see how much work it would be to extract all of the data.
+            ## Dirty Data?
+            Well, some of the columns were in formats that presented challenges. It was exciting but time-consuming.
+            ### The 'Dictionaries'
+            Several of the columns were in what I thought to be dictionaries. 
+            They were in fact JSON format, but some of them did not play nice with the JSON package native to python, so they ultimately got left alone.
+            Something for me to revisit.
+            ### Feature engineering.
+            ##### Category
+            So, I had a good hunch that the category of the project would be predictive, so I was sure to extract that out of the JSON formatted columns.
+            When I did, I was left with the choice of which 'Category' identifier to keep. The model went crazy when it was given all of them. 
+            Just barely beating baseline.
+            The identifier I went with was "category_name" and the reason, besides retaining predictive power,  was two-fold. 
+            One, I wanted a simple model that could be easily manipulated and interpreted by the user. 
+            Two I wanted to keep as much of the diverse info as possible from that group of features.
+            This allowed me to create an easy to read if long, drop-down of options to choose from. 
+            If I were not going to put this in an app like this, I would have also kept one of the parent category features.
+            ##### The dates
+            The dates we are given in this dataset are 'created', 'launched', and 'deadline'. 
+            From there, I converted them into a readable format and created more features. 
+            I created the campaign length, days_to_launch, which ended up being very important to the model. 
+            It's simple, but I was pretty happy to see that days to launch was useful. 
+            I had only recently learned that you could create and share a project on Kickstarter before actually launching it.
+            This gives you time to advertise, and to let the community help you work out perks, and goals. 
+            The other features I created were the month, day, year, and day name of the launch and deadline dates.
+            My intuition was that some times of the month/year would significantly increase your odds of success. 
+            This did not hold true, however.
+            ##### International
+            "Sir Not Appearing in this Film" There was a column that held more predictive power than some of the ones that made the cut, 
+            and the column was 'usd_type'. The binary values of this column were 'domestic' and 'international'. It is unclear whether this meant that the creator allowed 
+            shipping overseas or if this was a project that originated overseas, as there were many observations that overlapped. 
+            Though, I suspect that there should not have been.
+ 
+            ### XGBClassifier 
+            I chose a tree-based model because I really like the feature interaction detection that comes baked into them. 
+            Though, given the amount of time I spent training and retraining my models, I think that I should work with a non-boosted model first next time.
+            Just until I get the results that I want, then take my best few cases to the boosted model to see what is best.
+            This may end up saving me time. Precious time I can spend on EDA.
+ 
             """
         ),
-
+ 
     ],
 )
-
+ 
 layout = dbc.Row([column1])
+
